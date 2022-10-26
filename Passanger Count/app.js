c85b1898ef8a4147b893919d1980c59b6c6e0fc7
Muhammed Can Cetin <cmuhammed5300@gmail.com>
2022-10-26 17:02:31 +0300

    aligment updated

--- Passanger Count/app.js
+++ Passanger Count/app.js
@@ -1,23 +1,23 @@
 
-let countEl=document.getElementById("count")
-let saveEl= document.getElementById("storage")
-let count=0
+let countEl = document.getElementById("count")
+let saveEl = document.getElementById("storage")
+let count = 0
 
 
 
 
-function Increment(){
-    countEl.textContent=++count
+function Increment() {
+    countEl.textContent = ++count
 }
 
-function save(){
-    let countStr= count + " - "
-    saveEl.textContent+=countStr
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
     reset()
 }
 
 
-function reset(){
-    count=0
-    countEl.innerText=0
+function reset() {
+    count = 0
+    countEl.innerText = 0
 }
