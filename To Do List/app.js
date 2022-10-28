80ed371beb142ad17cbac5e137f00c0db25bf3a5
Muhammed Can Cetin <cmuhammed5300@gmail.com>
2022-10-28 20:46:22 +0300

    project completed

--- To Do List/app.js
+++ To Do List/app.js
@@ -3,34 +3,46 @@ const inputEl = document.getElementById("input-el");
 const addBtn = document.getElementById("liveToastBtn");
 const ulEl = document.getElementById("list");
 
-inputEl.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
+inputEl.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
         addBtn.click();
     }
 });
 
+ulEl.addEventListener("click", (e) => {
+    const listEl = e.target;
+    listEl.classList.toggle("checked");
+    if (listEl.classList[0] === "material-symbols-outlined") {
+        listEl.parentElement.classList.add("fall");
+    }
+})
 
-addBtn.addEventListener("click", function () {
+
+addBtn.addEventListener("click", () => {
     if (!isEmpty(inputEl.value)) {
         todo.push(inputEl.value);
     }
     else {
         alert("You cannot add empty object!");
     }
-    renderToDo();
+    add();
     inputEl.value = ""
 });
 
-function renderToDo() {
-    let listItems = "";
-    todo.forEach(element => {
-        listItems += `<li class="list-item">${element}</li>`
-    })
-    ulEl.innerHTML = listItems
+function add() {
+    let div = document.createElement("div")
+    div.innerHTML = `
+        <li class="list-item">${todo[todo.length - 1]}</li>
+        <span class="material-symbols-outlined close-icon">close</span>
+    `
+    div.classList.add("todo");
+    ulEl.append(div)
 }
 
 function isEmpty(str) {
     return !str.trim().length;
 }
+
+
 
